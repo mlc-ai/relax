be8b1b3888d9847d03a186886780f02d016891ab
Ruihang Lai <ruihangl@cs.cmu.edu>
2022-10-21 09:09:10 -0700

    relax.sqrt

--- tests/python/relax/test_relax_tensor_ops.py
+++ tests/python/relax/test_relax_tensor_ops.py
@@ -417,5 +417,21 @@ def test_layer_norm():
     tvm.ir.assert_structural_equal(bb.get()["main"], expected)
 
 
+def test_sqrt():
+    @R.function
+    def expected(x: R.Tensor((2, 3), "float32")) -> R.Tensor(None, "float32", ndim=2):
+        gv: R.Tensor((2, 3), "float32") = R.sqrt(x)
+        return gv
+
+    x = relax.Var("x", [2, 3], relax.DynTensorType(ndim=2, dtype="float32"))
+    bb = relax.BlockBuilder()
+    with bb.function("main", [x]):
+        gv = bb.emit(relax.op.sqrt(x))
+        bb.emit_func_output(gv)
+
+    expected = expected.with_attr("global_symbol", "main")
+    tvm.ir.assert_structural_equal(bb.get()["main"], expected)
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
