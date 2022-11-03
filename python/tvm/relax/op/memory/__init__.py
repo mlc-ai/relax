a39c9ea8f35f9700da6118c734f1b94d6c9f81cf
Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
2022-11-03 08:16:35 -0700

    [Cherry-pick] Introduce memory primitives (tlc-pack#255)
    Introduce the memory primitives, including `relax.memory.{alloc_storage, alloc_tensor, kill_storage, kill_tensor}`.

--- python/tvm/relax/op/memory/__init__.py
+++ python/tvm/relax/op/memory/__init__.py
@@ -15,15 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 # pylint: disable=wildcard-import, redefined-builtin
-"""Relax core operators."""
+"""Relax memory primitives."""
 
-# Operators
-from .base import *
-from .image import *
-from .tensor import *
-from .op_attrs import *
-from .reduce import *
-from .transform import *
-from .nn import *
-from . import builtin
-from . import memory
+from .memory import *
