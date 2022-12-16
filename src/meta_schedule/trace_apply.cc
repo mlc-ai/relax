4ae0e05cf196789e6f7a55b5bcb6320f4cbcc8c4
jinhongyi <3231950289@qq.com>
2022-12-16 14:52:15 -0800

    fix trace apply finally

--- src/meta_schedule/trace_apply.cc
+++ src/meta_schedule/trace_apply.cc
@@ -48,7 +48,8 @@ bool IsAncestor(BlockRV b1, BlockRV b2, Schedule sch) {
 }
 
 // Inline or reverse inline spatial blocks after the anchor block
-void InlinePostBlocks(Schedule sch, Trace anchor_trace, Target target) {
+void InlinePostBlocks(Schedule sch, Trace anchor_trace, Target target,
+                      Map<String, String> target2record_block_mapping) {
   static auto kind_get_block = InstructionKind::Get("GetBlock");
   // We let blocks whose names are referenced in the anchor trace be scheduled by the anchor trace.
   // We record such block names to avoid inlining them here.
@@ -72,9 +73,9 @@ void InlinePostBlocks(Schedule sch, Trace anchor_trace, Target target) {
       auto anchor_block_rv = sch->GetBlock(anchor_block->name_hint);
       if (IsAncestor(block, anchor_block_rv, sch)) continue;
     }
-    // Spatial blocks which are not referenced in the anchor trace will be inlined here.
+    // Spatial blocks which are not mapped between record mod and target mod will be inlined here.
     auto block_sref = sch->GetSRef(block);
-    if (IsSpatial(block_sref) && !get_block_names.count(name)) {
+    if (IsSpatial(block_sref) && !target2record_block_mapping.count(name)) {
       if (IsOutputBlock(sch->state(), block_sref, GetScopeRoot(sch->state(), block_sref, false))) {
         last_block_idx = inline_todos.size();
       }
@@ -95,54 +96,82 @@ void InlinePostBlocks(Schedule sch, Trace anchor_trace, Target target) {
   }
 }
 
-// Apply instructions from the anchor trace to the target schedule, and returns blocks
-// that remain unscheduled.
-std::vector<BlockRV> ApplyAnchorTrace(Schedule sch, Trace anchor_trace,
-                                      const IRModule& record_mod) {
-  // Extract all blocks from a module
-  class BlockExtractor : public StmtExprVisitor {
-   public:
-    static Array<Block> ExtractBlocks(const IRModule& mod) {
-      PrimFunc func = Downcast<PrimFunc>(mod->Lookup("main"));
-      BlockExtractor extractor;
-      extractor(func->body);
-      return extractor.blocks;
-    }
+// Same to Structural Equal except ignoring float constant difference
+class SEqualHandlerIgnoreFloat : public SEqualHandlerDefault {
+ public:
+  SEqualHandlerIgnoreFloat() : SEqualHandlerDefault(false, nullptr) {}
 
-   private:
-    void VisitStmt_(const BlockNode* block) final {
-      if (block->name_hint == "root") {
-        StmtExprVisitor::VisitStmt_(block);
-        return;
-      }
-      ICHECK(!in_block_) << "Nested blocks are not supported";
-      in_block_ = true;
-      blocks.push_back(GetRef<Block>(block));
-      StmtExprVisitor::VisitStmt_(block);
-      in_block_ = false;
+ protected:
+  bool DispatchSEqualReduce(const ObjectRef& lhs, const ObjectRef& rhs, bool map_free_vars,
+                            const Optional<ObjectPathPair>& current_paths) {
+    if (auto lhs_ptr = lhs.as<FloatImmNode>(), rhs_ptr = rhs.as<FloatImmNode>();
+        lhs_ptr && rhs_ptr) {
+      return true;
     }
-    Array<Block> blocks;
-    bool in_block_ = false;
-  };
-  Array<Block> blocks_from_record = BlockExtractor::ExtractBlocks(record_mod);
-  Array<Block> blocks_from_target = BlockExtractor::ExtractBlocks(sch->mod());
-  // match same blocks
-  // block maps between target mod and record mod
+    return SEqualHandlerDefault::DispatchSEqualReduce(lhs, rhs, map_free_vars, current_paths);
+  }
+};
+
+// Extract all blocks from a module
+class BlockExtractor : public StmtExprVisitor {
+ public:
+  static Array<Block> ExtractBlocks(Stmt stmt) {
+    BlockExtractor extractor;
+    extractor(stmt);
+    return extractor.blocks;
+  }
+
+ private:
+  void VisitStmt_(const BlockNode* block) final {
+    if (block->name_hint == "root") {
+      StmtExprVisitor::VisitStmt_(block);
+      return;
+    }
+    ICHECK(!in_block_) << "Nested blocks are not supported";
+    in_block_ = true;
+    blocks.push_back(GetRef<Block>(block));
+    StmtExprVisitor::VisitStmt_(block);
+    in_block_ = false;
+  }
+  Array<Block> blocks;
+  bool in_block_ = false;
+};
+
+// Map blocks between record mod and target mod
+std::pair<Map<String, String>, Map<String, String>> GetBlockMapping(IRModule target_mod,
+                                                                    IRModule record_mod) {
+  PrimFunc record_func = Downcast<PrimFunc>(record_mod->Lookup("main"));
+  Array<Block> blocks_from_record = BlockExtractor::ExtractBlocks(record_func->body);
+  PrimFunc target_func = Downcast<PrimFunc>(target_mod->Lookup("main"));
+  Array<Block> blocks_from_target = BlockExtractor::ExtractBlocks(target_func->body);
+  // Map blocks between record mod and target mod
   Map<String, String> record2target{{"root", "root"}};
   Map<String, String> target2record{{"root", "root"}};
-  for (int i = 0; i < static_cast<int>(blocks_from_record.size()); i++) {
-    for (int j = 0; j < static_cast<int>(blocks_from_target.size()); j++) {
-      if (tvm::StructuralEqual()(blocks_from_record[i], blocks_from_target[j])) {
+
+  for (int i = 0; i < blocks_from_record.size(); i++) {
+    for (int j = 0; j < blocks_from_target.size(); j++) {
+      if (SEqualHandlerIgnoreFloat().Equal(blocks_from_record[i], blocks_from_target[j], false) &&
+          !target2record.count(blocks_from_target[j]->name_hint)) {
         record2target.Set(blocks_from_record[i]->name_hint, blocks_from_target[j]->name_hint);
         target2record.Set(blocks_from_target[j]->name_hint, blocks_from_record[i]->name_hint);
+        break;
       }
     }
   }
+  return std::make_pair(target2record, record2target);
+}
+
+// Apply instructions from the anchor trace to the target schedule, and returns blocks
+// that remain unscheduled.
+std::vector<BlockRV> ApplyAnchorTrace(Schedule sch, Trace anchor_trace, const IRModule& record_mod,
+                                      Map<String, String> record2target,
+                                      Map<String, String> target2record) {
   static auto kind_get_child_blocks = InstructionKind::Get("GetChildBlocks");
   static auto kind_get_block = InstructionKind::Get("GetBlock");
   static auto kind_compute_inline = InstructionKind::Get("ComputeInline");
   static auto kind_reverse_compute_inline = InstructionKind::Get("ReverseComputeInline");
   static auto kind_decompose_reduction = InstructionKind::Get("DecomposeReduction");
+  static auto kind_blockize = InstructionKind::Get("Blockize");
 
   const auto block_names_orig = GetBlockNames(sch->mod());
   const auto sch_orig = sch->Copy();
@@ -232,12 +261,9 @@ std::vector<BlockRV> ApplyAnchorTrace(Schedule sch, Trace anchor_trace,
     Array<ObjectRef> outputs = inst->kind->f_apply_to_schedule(sch, inputs, attrs, decision);
 
     if (inst->kind.same_as(kind_get_child_blocks)) {
-      // We want to allow a trace generated for a single conv2d block to be applied to
-      // conv2d -> elemwise blocks, where two conv2d are the same workload.
-      // GetChildBlocks returns a different number of blocks for the two cases above, which
-      // violates the assumption made by TranslateAddOutputRVs: old_outputs.size() ==
-      // new_outputs.size(). We workaround this problem by assuming that the prefix of the "new"
-      // outputs matches with the "old" outputs, and truncating the new outputs accordingly.
+      BlockRV input_rv = Downcast<BlockRV>(inputs[0]);
+      Block input_block = Downcast<Block>(sch->Get(input_rv));
+      // only add the mapped blocks to the output rvs
       Array<ObjectRef> new_target_outputs;
       for (int i = 0; i < static_cast<int>(outputs.size()); i++) {
         auto block_target = sch->Get(Downcast<BlockRV>(outputs[i]));
@@ -255,36 +281,52 @@ std::vector<BlockRV> ApplyAnchorTrace(Schedule sch, Trace anchor_trace,
         }
       }
       TranslateAddOutputRVs(new_inst_outputs, new_target_outputs, &rv_map_target_mod);
-    } else {
-      // map record mod block_rvs to target mod block_rvs
-      ICHECK(outputs.size() == outputs_for_record_mod.size());
-      for (int i = 0; i < static_cast<int>(outputs.size()); i++) {
-        if (outputs[i]->IsInstance<BlockRVNode>()) {
-          ICHECK(outputs_for_record_mod[i]->IsInstance<BlockRVNode>());
-          auto block_rv_record = Downcast<BlockRV>(outputs_for_record_mod[i]);
+      continue;
+    }
+    // maintain block mapping
+    ICHECK(outputs.size() == outputs_for_record_mod.size());
+    for (int i = 0; i < static_cast<int>(outputs.size()); i++) {
+      if (outputs[i]->IsInstance<BlockRVNode>()) {
+        ICHECK(outputs_for_record_mod[i]->IsInstance<BlockRVNode>());
+        auto block_rv_record = Downcast<BlockRV>(outputs_for_record_mod[i]);
+        auto block_record = Downcast<Block>(sch_for_record_mod->Get(block_rv_record));
+        auto block_rv_target = Downcast<BlockRV>(outputs[i]);
+        auto block_target = Downcast<Block>(sch->Get(block_rv_target));
+        record2target.Set(block_record->name_hint, block_target->name_hint);
+        target2record.Set(block_target->name_hint, block_record->name_hint);
+      }
+    }
+    if (inst->kind.same_as(kind_decompose_reduction)) {
+      // decompose_reduction will modify input block
+      ICHECK(inputs.size() == inputs_for_record_mod.size());
+      for (int i = 0; i < static_cast<int>(inputs.size()); i++) {
+        if (inputs[i]->IsInstance<BlockRVNode>()) {
+          auto block_rv_record = Downcast<BlockRV>(inputs_for_record_mod[i]);
           auto block_record = Downcast<Block>(sch_for_record_mod->Get(block_rv_record));
-          auto block_rv_target = Downcast<BlockRV>(outputs[i]);
+          auto block_rv_target = Downcast<BlockRV>(inputs[i]);
           auto block_target = Downcast<Block>(sch->Get(block_rv_target));
           record2target.Set(block_record->name_hint, block_target->name_hint);
           target2record.Set(block_target->name_hint, block_record->name_hint);
         }
       }
-      // decompose reduction will modify input block
-      if (inst->kind.same_as(kind_decompose_reduction)) {
-        ICHECK(inputs.size() == inputs_for_record_mod.size());
-        for (int i = 0; i < static_cast<int>(inputs.size()); i++) {
-          if (inputs[i]->IsInstance<BlockRVNode>()) {
-            auto block_rv_record = Downcast<BlockRV>(inputs_for_record_mod[i]);
-            auto block_record = Downcast<Block>(sch_for_record_mod->Get(block_rv_record));
-            auto block_rv_target = Downcast<BlockRV>(inputs[i]);
-            auto block_target = Downcast<Block>(sch->Get(block_rv_target));
-            record2target.Set(block_record->name_hint, block_target->name_hint);
-            target2record.Set(block_target->name_hint, block_record->name_hint);
-          }
-        }
+    } else if (inst->kind.same_as(kind_blockize)) {
+      // blockize will generate a block under the init block
+      Block target_output_block = Downcast<Block>(sch->Get(Downcast<BlockRV>(outputs[0])));
+      if (target_output_block->init.defined()) {
+        Array<Block> target_init_blocks =
+            BlockExtractor::ExtractBlocks(target_output_block->init.value());
+        Block record_output_block =
+            Downcast<Block>(sch_for_record_mod->Get(Downcast<BlockRV>(outputs_for_record_mod[0])));
+        ICHECK(record_output_block->init.defined());
+        Array<Block> record_init_blocks =
+            BlockExtractor::ExtractBlocks(record_output_block->init.value());
+        ICHECK(target_init_blocks.size() == 1);
+        ICHECK(record_init_blocks.size() == 1);
+        record2target.Set(record_init_blocks[0]->name_hint, target_init_blocks[0]->name_hint);
+        target2record.Set(target_init_blocks[0]->name_hint, record_init_blocks[0]->name_hint);
       }
-      TranslateAddOutputRVs(inst->outputs, outputs, &rv_map_target_mod);
     }
+    TranslateAddOutputRVs(inst->outputs, outputs, &rv_map_target_mod);
   }
 
   auto is_scheduled = [=](const std::string& block_name) {
@@ -316,9 +358,12 @@ std::vector<BlockRV> ApplyAnchorTrace(Schedule sch, Trace anchor_trace,
 
 void ScheduleUsingAnchorTrace(Schedule sch, const Trace& anchor_trace, const IRModule& record_mod,
                               const tvm::Target& target) {
-  InlinePostBlocks(sch, anchor_trace, target);
+  Map<String, String> record2target, target2record;
+  std::tie(target2record, record2target) = GetBlockMapping(sch->mod(), record_mod);
+  InlinePostBlocks(sch, anchor_trace, target, target2record);
 
-  auto unscheduled_blocks = ApplyAnchorTrace(sch, anchor_trace, record_mod);
+  auto unscheduled_blocks =
+      ApplyAnchorTrace(sch, anchor_trace, record_mod, record2target, target2record);
   ICHECK(unscheduled_blocks.size() <= 1)
       << "All blocks should have been scheduled or only one (fused) spatial block can remain "
          "unscheduled at this point.";
--- tests/python/unittest/test_meta_schedule_trace_apply.py
+++ tests/python/unittest/test_meta_schedule_trace_apply.py
@@ -85,6 +85,40 @@ class DenseAdd:
                 T.writes(T_add[ax0, ax1])
                 T_add[ax0, ax1] = T_matmul_NT[ax0, ax1] + compile_engine_const[()]
 
+@tvm.script.ir_module
+class DenseAdd_different_name:
+    @T.prim_func
+    def main(
+        p00: T.Buffer[(128, 128), "float32"],
+        p10: T.Buffer[(128, 128), "float32"],
+        T_add_: T.Buffer[(128, 128), "float32"],
+    ) -> None:
+        # function attr dict
+        T.func_attr({"global_symbol": "main", "tir.noalias": True, "layout_free_buffers": [1]})
+        # body
+        # with T.block("root")
+        T_matmul_NT_ = T.alloc_buffer([128, 128], dtype="float32")
+        compile_engine_const_ = T.alloc_buffer([], dtype="float32")
+        for i0, i1, i2 in T.grid(128, 128, 128):
+            with T.block("T_matmul_NT_"):
+                i, j, k = T.axis.remap("SSR", [i0, i1, i2])
+                T.reads(p00[i, k], p10[j, k])
+                T.writes(T_matmul_NT_[i, j])
+                T.block_attr({"layout_free_placeholders": []})
+                with T.init():
+                    T_matmul_NT_[i, j] = T.float32(0)
+                T_matmul_NT_[i, j] = T_matmul_NT_[i, j] + p00[i, k] * p10[j, k]
+        with T.block("compile_engine_const_"):
+            vi = T.axis.spatial(1, 0)
+            T.reads()
+            T.writes(compile_engine_const_[()])
+            compile_engine_const_[()] = T.float32(1)
+        for i0, i1 in T.grid(128, 128):
+            with T.block("T_add_"):
+                ax0, ax1 = T.axis.remap("SS", [i0, i1])
+                T.reads(T_matmul_NT_[ax0, ax1], compile_engine_const_[()])
+                T.writes(T_add_[ax0, ax1])
+                T_add_[ax0, ax1] = T_matmul_NT_[ax0, ax1] + compile_engine_const_[()]
 
 @tvm.script.ir_module
 class DenseAdd_scheduled_cpu:
@@ -1821,8 +1855,7 @@ def verify(anchor_mod, anchor_trace_fun, target_mod, target, ref):
 
     sch = Schedule(target_mod)
 
-    ms.trace_apply.schedule_using_anchor_trace(sch, anchor_trace, Target(target))
-
+    ms.trace_apply.schedule_using_anchor_trace(sch, anchor_trace, anchor_mod, Target(target))
     tvm.ir.assert_structural_equal(ref, sch.mod)
 
 
@@ -3370,6 +3403,132 @@ def test_inline_order():
         "cuda",
         Conv2dInt8_with_predicate_scheduled,
     )
+
+
+def test_dense_add_gpu_different_name():
+    def apply_anchor_trace(sch: Schedule) -> None:
+        b0 = sch.get_block(name="T_matmul_NT", func_name="main")
+        b1 = sch.get_block(name="root", func_name="main")
+        sch.annotate(
+            block_or_loop=b0, ann_key="meta_schedule.tiling_structure", ann_val="SSSRRSRS")
+        l2, l3, l4 = sch.get_loops(block=b0)
+        v5, v6, v7, v8, v9 = sch.sample_perfect_tile(
+            loop=l2, n=5, max_innermost_factor=64, decision=[8, 1, 16, 1, 1]
+        )
+        l10, l11, l12, l13, l14 = sch.split(
+            loop=l2, factors=[v5, v6, v7, v8, v9], preserve_unit_iters=True
+        )
+        v15, v16, v17, v18, v19 = sch.sample_perfect_tile(
+            loop=l3, n=5, max_innermost_factor=64, decision=[4, 1, 8, 4, 1]
+        )
+        l20, l21, l22, l23, l24 = sch.split(
+            loop=l3, factors=[v15, v16, v17, v18,
+                              v19], preserve_unit_iters=True
+        )
+        v25, v26, v27 = sch.sample_perfect_tile(
+            loop=l4, n=3, max_innermost_factor=64, decision=[32, 1, 4]
+        )
+        l28, l29, l30 = sch.split(
+            loop=l4, factors=[v25, v26, v27], preserve_unit_iters=True)
+        sch.reorder(l10, l20, l11, l21, l12, l22,
+                    l28, l29, l13, l23, l30, l14, l24)
+        l31 = sch.fuse(l10, l20, preserve_unit_iters=True)
+        sch.bind(loop=l31, thread_axis="blockIdx.x")
+        l32 = sch.fuse(l11, l21, preserve_unit_iters=True)
+        sch.bind(loop=l32, thread_axis="vthread.x")
+        l33 = sch.fuse(l12, l22, preserve_unit_iters=True)
+        sch.bind(loop=l33, thread_axis="threadIdx.x")
+        sch.annotate(
+            block_or_loop=b0, ann_key="meta_schedule.thread_extent_low_inclusive", ann_val=16
+        )
+        sch.annotate(
+            block_or_loop=b0, ann_key="meta_schedule.thread_extent_high_inclusive", ann_val=256
+        )
+        b34 = sch.cache_write(
+            block=b0, write_buffer_index=0, storage_scope="local")
+        sch.reverse_compute_at(block=b34, loop=l33,
+                               preserve_unit_loops=True, index=-1)
+        b35 = sch.cache_read(
+            block=b0, read_buffer_index=0, storage_scope="shared", consumer_blocks=[b0]
+        )
+        sch.compute_at(block=b35, loop=l28, preserve_unit_loops=True, index=-1)
+        l36, l37, l38, l39, l40, l41 = sch.get_loops(block=b35)
+        l42 = sch.fuse(l40, l41, preserve_unit_iters=True)
+        v43 = sch.sample_categorical(
+            candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25], decision=1
+        )
+        sch.annotate(block_or_loop=b35,
+                     ann_key="meta_schedule.cooperative_fetch", ann_val=v43)
+        b44 = sch.cache_read(
+            block=b0, read_buffer_index=1, storage_scope="shared", consumer_blocks=[b0]
+        )
+        sch.compute_at(block=b44, loop=l28, preserve_unit_loops=True, index=-1)
+        l45, l46, l47, l48, l49, l50 = sch.get_loops(block=b44)
+        l51 = sch.fuse(l49, l50, preserve_unit_iters=True)
+        v52 = sch.sample_categorical(
+            candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25], decision=3
+        )
+        sch.annotate(block_or_loop=b44,
+                     ann_key="meta_schedule.cooperative_fetch", ann_val=v52)
+        v53 = sch.sample_categorical(
+            candidates=[0, 16, 64, 512, 1024],
+            probs=[
+                0.20000000000000001,
+                0.20000000000000001,
+                0.20000000000000001,
+                0.20000000000000001,
+                0.20000000000000001,
+            ],
+            decision=2,
+        )
+        sch.annotate(block_or_loop=b1,
+                     ann_key="meta_schedule.unroll_explicit", ann_val=v53)
+        sch.enter_postproc()
+        sch.unannotate(block_or_loop=b35,
+                       ann_key="meta_schedule.cooperative_fetch")
+        l54, l55, l56, l57, l58 = sch.get_loops(block=b35)
+        l59, l60, l61 = sch.split(
+            loop=l58, factors=[None, 128, 2], preserve_unit_iters=True)
+        sch.vectorize(loop=l61)
+        sch.bind(loop=l60, thread_axis="threadIdx.x")
+        sch.unannotate(block_or_loop=b44,
+                       ann_key="meta_schedule.cooperative_fetch")
+        l62, l63, l64, l65, l66 = sch.get_loops(block=b44)
+        l67, l68, l69 = sch.split(
+            loop=l66, factors=[None, 128, 4], preserve_unit_iters=True)
+        sch.vectorize(loop=l69)
+        sch.bind(loop=l68, thread_axis="threadIdx.x")
+        b70 = sch.get_block(name="root", func_name="main")
+        sch.unannotate(block_or_loop=b70,
+                       ann_key="meta_schedule.unroll_explicit")
+        b71, b72, b73, b74 = sch.get_child_blocks(b70)
+        l75, l76, l77, l78, l79, l80, l81 = sch.get_loops(block=b71)
+        sch.annotate(block_or_loop=l75,
+                     ann_key="pragma_auto_unroll_max_step", ann_val=64)
+        sch.annotate(block_or_loop=l75,
+                     ann_key="pragma_unroll_explicit", ann_val=1)
+        l82, l83, l84, l85, l86, l87, l88 = sch.get_loops(block=b72)
+        sch.annotate(block_or_loop=l82,
+                     ann_key="pragma_auto_unroll_max_step", ann_val=64)
+        sch.annotate(block_or_loop=l82,
+                     ann_key="pragma_unroll_explicit", ann_val=1)
+        l89, l90, l91, l92, l93, l94, l95, l96, l97, l98 = sch.get_loops(
+            block=b73)
+        sch.annotate(block_or_loop=l89,
+                     ann_key="pragma_auto_unroll_max_step", ann_val=64)
+        sch.annotate(block_or_loop=l89,
+                     ann_key="pragma_unroll_explicit", ann_val=1)
+        l99, l100, l101, l102, l103 = sch.get_loops(block=b74)
+        sch.annotate(block_or_loop=l99,
+                     ann_key="pragma_auto_unroll_max_step", ann_val=64)
+        sch.annotate(block_or_loop=l99,
+                     ann_key="pragma_unroll_explicit", ann_val=1)
+        b104 = sch.get_block(name="T_matmul_NT", func_name="main")
+        l105, l106, l107, l108, l109, l110, l111, l112, l113, l114 = sch.get_loops(
+            block=b104)
+        b115 = sch.decompose_reduction(block=b104, loop=l108)
+
+    verify(Dense, apply_anchor_trace, DenseAdd_different_name, "cuda", DenseAdd_scheduled_gpu)
 
 
 if __name__ == "__main__":
