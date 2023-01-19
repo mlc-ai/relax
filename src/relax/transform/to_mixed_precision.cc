34ae8d64f642dbd69a4f7399bb60b758f77b5d81
Tianqi Chen <tqchen@users.noreply.github.com>
2023-01-19 16:43:50 -0500

    Revert "[MLC][CI] Try to setup win mac building CI" (#105)
    Reverts mlc-ai/relax#104

--- src/relax/transform/to_mixed_precision.cc
+++ src/relax/transform/to_mixed_precision.cc
@@ -25,8 +25,6 @@
 #include <tvm/relax/op_attr_types.h>
 #include <tvm/relax/transform.h>
 
-#include <array>
-
 #include "../op/nn/convolution.h"
 #include "../op/tensor/datatype.h"
 #include "../op/tensor/linear_algebra.h"
