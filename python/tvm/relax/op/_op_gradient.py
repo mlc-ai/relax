ef4993463940958e554fb6870ce13658c1df4f9b
Chaosfan <siriusneo@sjtu.edu.cn>
2023-02-13 22:07:06 +0800

    [Op][Gradient] More gradients and polishment (#126)
    This PR introduces more gradients for operators and polishes the
    structure and tests. To be specific,
    - Overload `__neg__` for relax expr.
    - Complete the gradients from unary, binary operators.
    - Use overloaded operators instead of function in the implementation to
    make the calculation more clear. (E.g. use `a + b` instead of `add(a,
    b)`)
    - Add the annotation of the return value. (`List[Expr]`)
    - Refactor tests.

--- python/tvm/relax/op/_op_gradient.py
+++ python/tvm/relax/op/_op_gradient.py
@@ -27,20 +27,18 @@ from ...tir import PrimExpr
 from .base import register_gradient
 
 from .unary import (
-    log,
-    negative,
+    cos,
     exp,
+    log,
+    sin,
+    sqrt,
 )
-from .binary import (
-    subtract,
-    multiply,
-    divide,
-    less,
-)
+from .binary import less
 from .statistical import sum
 from .create import (
     zeros,
     ones,
+    zeros_like,
 )
 from .search import where
 from .linear_algebra import matmul
@@ -66,6 +64,25 @@ def _get_shape(expr: Expr) -> ShapeExpr:
     return shape
 
 
+def _get_dtype(expr: Expr) -> str:
+    """Get the dtype from a Tensor expr."""
+    try:
+        dtype = expr.struct_info.dtype
+    except Exception as error:
+        raise TVMError(
+            f"Get the dtype of {expr} failed. Please normalize it first and ensure it is a Tensor."
+        ) from error
+    return dtype
+
+
+def _ones(expr: Expr) -> Expr:
+    return ones(_get_shape(expr), _get_dtype(expr))
+
+
+def _zeros(expr: Expr) -> Expr:
+    return zeros(_get_shape(expr), _get_dtype(expr))
+
+
 def _fit_shape(expr: Expr, expr_shape: ShapeExpr, target: Expr) -> Expr:
     target_shape = _get_shape(target)
     expr_sinfo = expr_shape.struct_info
@@ -85,14 +102,16 @@ def _fit_shape(expr: Expr, expr_shape: ShapeExpr, target: Expr) -> Expr:
     return expr if _check_shape_equal() else collapse_sum_to(expr, target_shape)
 
 
-# Arith
+##################### Binary #####################
+
+
 @register_gradient("relax.add")
 def add_grad(
     orig_var: Var,
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of add.
 
     Forward Form:
@@ -114,7 +133,7 @@ def subtract_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of subtract.
 
     Forward Form:
@@ -126,7 +145,7 @@ def subtract_grad(
     output_grad_shape = _get_shape(output_grad)
     return [
         _fit_shape(output_grad, output_grad_shape, orig_call.args[0]),
-        _fit_shape(negative(output_grad), output_grad_shape, orig_call.args[1]),
+        _fit_shape(-output_grad, output_grad_shape, orig_call.args[1]),
     ]
 
 
@@ -136,7 +155,7 @@ def multiply_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of multiply.
 
     Forward Form:
@@ -148,9 +167,229 @@ def multiply_grad(
     x, y = orig_call.args
     output_grad_shape = _get_shape(output_grad)
     return [
-        _fit_shape(multiply(output_grad, y), output_grad_shape, x),
-        _fit_shape(multiply(output_grad, x), output_grad_shape, y),
+        _fit_shape(output_grad * y, output_grad_shape, x),
+        _fit_shape(output_grad * x, output_grad_shape, y),
     ]
+
+
+@register_gradient("relax.divide")
+def divide_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of divide.
+
+    Forward Form:
+        z = relax.divide(x, y)
+
+    Backward:
+        Returns [z_grad / y,  - z_grad * z / y].
+    """
+    x, y = orig_call.args
+    output_grad_shape = _get_shape(output_grad)
+    return [
+        _fit_shape(output_grad / y, output_grad_shape, x),
+        _fit_shape(-output_grad * orig_var / y, output_grad_shape, y),
+    ]
+
+
+# TODO(chaofan): floor-divide
+
+# For most comparison operators, the gradients are just zeros.
+def _binary_zeros(call: Call) -> List[Expr]:
+    return [_zeros(call.args[0]), _zeros(call.args[1])]
+
+
+@register_gradient("relax.equal")
+def equal_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return _binary_zeros(orig_call)
+
+
+@register_gradient("relax.greater")
+def greater_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return _binary_zeros(orig_call)
+
+
+@register_gradient("relax.greater_equal")
+def greater_equal_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return _binary_zeros(orig_call)
+
+
+@register_gradient("relax.less")
+def less_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return _binary_zeros(orig_call)
+
+
+@register_gradient("relax.less_equal")
+def less_equal_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return _binary_zeros(orig_call)
+
+
+@register_gradient("relax.not_equal")
+def not_equal_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return _binary_zeros(orig_call)
+
+
+##################### Create #####################
+
+
+# For these create operators, the gradients are just zeros.
+@register_gradient("relax.zeros_like")
+def zeros_like_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return [orig_var]
+
+
+@register_gradient("relax.ones_like")
+def ones_like_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return [zeros_like(orig_call.args[0], orig_call.attrs.dtype)]
+
+
+@register_gradient("relax.full_like")
+def full_like_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    return [zeros_like(orig_call.args[0], orig_call.attrs.dtype)]
+
+
+##################### Unary #####################
+
+
+@register_gradient("relax.abs")
+def abs_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of abs.
+
+    Forward Form:
+        y = relax.abs(x)
+
+    Backward:
+        Returns [y_grad * where(x < 0, -1, 1)].
+    """
+    x = orig_call.args[0]
+    x_zeros = _zeros(x)
+    x_ones = _ones(x)
+    return [output_grad * where(less(x, x_zeros), -x_ones, x_ones)]
+
+
+@register_gradient("relax.cos")
+def cos_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of cos.
+
+    Forward Form:
+        y = relax.cos(x)
+
+    Backward:
+        Returns [-y_grad * sin(x)].
+    """
+    return [-output_grad * sin(orig_call.args[0])]
+
+
+@register_gradient("relax.exp")
+def exp_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of exp.
+
+    Forward Form:
+        y = relax.exp(x)
+
+    Backward:
+        Returns [y_grad * y].
+    """
+    return [output_grad * orig_var]
+
+
+@register_gradient("relax.log")
+def log_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of log.
+
+    Forward Form:
+        y = relax.log(x)
+
+    Backward:
+        Returns [y_grad / x].
+    """
+    return [output_grad / orig_call.args[0]]
+
+
+@register_gradient("relax.negative")
+def negative_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of negative.
+
+    Forward Form:
+        y = relax.negative(x)
+
+    Backward:
+        Returns [- y_grad].
+    """
+    return [-output_grad]
 
 
 @register_gradient("relax.sigmoid")
@@ -159,7 +398,7 @@ def sigmoid_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of sigmoid.
 
     Forward Form:
@@ -168,13 +407,46 @@ def sigmoid_grad(
     Backward:
         Returns [y_grad * y * (1 - y)].
     """
-    x_ones = ones(_get_shape(orig_call.args[0]), orig_call.args[0].struct_info.dtype)
-    return [
-        multiply(
-            output_grad,
-            multiply(orig_var, subtract(x_ones, orig_var)),
-        )
-    ]
+    x_ones = _ones(orig_call.args[0])
+    return [output_grad * orig_var * (x_ones - orig_var)]
+
+
+@register_gradient("relax.sin")
+def sin_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of sin.
+
+    Forward Form:
+        y = relax.sin(x)
+
+    Backward:
+        Returns [y_grad * cos(x)].
+    """
+    return [output_grad * cos(orig_call.args[0])]
+
+
+@register_gradient("relax.sqrt")
+def sqrt_grad(
+    orig_var: Var,
+    orig_call: Call,
+    output_grad: Var,
+    ctx: BlockBuilder,
+) -> List[Expr]:
+    """Gradient of sqrt.
+
+    Forward Form:
+        y = relax.sqrt(x)
+
+    Backward:
+        Returns [0.5 * y_grad / sqrt(x)].
+    """
+    x = orig_call.args[0]
+    cst = relax.const(0.5, dtype=_get_dtype(x))
+    return [cst * output_grad / sqrt(x)]
 
 
 @register_gradient("relax.tanh")
@@ -183,7 +455,7 @@ def tanh_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of tanh.
 
     Forward Form:
@@ -192,23 +464,20 @@ def tanh_grad(
     Backward:
         Returns [y_grad * (1 - y * y)].
     """
-    x_ones = ones(_get_shape(orig_call.args[0]), orig_call.args[0].struct_info.dtype)
-    return [
-        multiply(
-            output_grad,
-            subtract(x_ones, multiply(orig_var, orig_var)),
-        )
-    ]
+    x_ones = _ones(orig_call.args[0])
+    return [output_grad * (x_ones - orig_var * orig_var)]
 
 
-# Statistical
+##################### Statistical #####################
+
+
 @register_gradient("relax.sum")
 def sum_grad(
     orig_var: Var,
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of sum.
 
     Forward Form:
@@ -225,14 +494,16 @@ def sum_grad(
     return [broadcast_to(output_grad, _get_shape(orig_call.args[0]))]
 
 
-# Manipulate
+##################### Manipulate #####################
+
+
 @register_gradient("relax.permute_dims")
 def permute_dims_grad(
     orig_var: Var,
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of permute_dims.
 
     Forward Form:
@@ -259,7 +530,7 @@ def concat_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of concat.
 
     Forward Form:
@@ -291,7 +562,7 @@ def split_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of split.
 
     Forward Form:
@@ -306,14 +577,16 @@ def split_grad(
     return [concat(output_grad, axis)]
 
 
-# Linear Algebra
+##################### Linear Algebra #####################
+
+
 @register_gradient("relax.matmul")
 def matmul_grad(
     orig_var: Var,
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of matmul.
 
     Forward Form:
@@ -354,8 +627,8 @@ def matmul_grad(
         )  # squeeze last dim
     else:
         assert a_dim == 1 and b_dim == 1
-        a_grad = multiply(output_grad, tensor_b)
-        b_grad = multiply(output_grad, tensor_a)
+        a_grad = output_grad * tensor_b
+        b_grad = output_grad * tensor_a
 
     output_grad_shape = _get_shape(output_grad)
 
@@ -365,14 +638,16 @@ def matmul_grad(
     ]
 
 
-# NN
+##################### Neural network #####################
+
+
 @register_gradient("relax.nn.relu")
 def relu_grad(
     orig_var: Var,
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of relu.
 
     Forward Form:
@@ -382,10 +657,9 @@ def relu_grad(
         Returns [y_grad * (where(x < 0, 0, 1))].
     """
     x = orig_call.args[0]
-    x_shape = _get_shape(x)
-    x_zeros = zeros(x_shape, x.struct_info.dtype)
-    x_ones = ones(x_shape, x.struct_info.dtype)
-    return [where(less(x, x_zeros), x_zeros, multiply(x_ones, output_grad))]
+    x_zeros = _zeros(x)
+    x_ones = _ones(x)
+    return [where(less(x, x_zeros), x_zeros, x_ones) * output_grad]
 
 
 @register_gradient("relax.nn.softmax")
@@ -394,7 +668,7 @@ def softmax_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of softmax.
 
     Forward Form:
@@ -403,12 +677,7 @@ def softmax_grad(
     Backward:
         Returns [(y_grad - sum(y_grad * y, axis, keepdims=True)) * y]
     """
-    return [
-        multiply(
-            subtract(output_grad, sum(multiply(output_grad, orig_var), orig_call.attrs.axis, True)),
-            orig_var,
-        )
-    ]
+    return [(output_grad - sum(output_grad * orig_var, orig_call.attrs.axis, True)) * orig_var]
 
 
 @register_gradient("relax.nn.log_softmax")
@@ -417,7 +686,7 @@ def log_softmax_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of log_softmax.
 
     Forward Form:
@@ -427,9 +696,7 @@ def log_softmax_grad(
         Returns [y_grad - sum(y_output_grad, axis, keepdims=True) * softmax(x)]
     """
     x_softmax = exp(orig_var)
-    return [
-        subtract(output_grad, multiply(sum(output_grad, orig_call.attrs.axis, True), x_softmax))
-    ]
+    return [(output_grad - sum(output_grad, orig_call.attrs.axis, True) * x_softmax)]
 
 
 def _divide_batch(x: Expr, expr: Expr):
@@ -439,7 +706,7 @@ def _divide_batch(x: Expr, expr: Expr):
         batch_size = int(x_shape[0])
         # batch_size = take(shape_of(x), relax.const(0, dtype="int32"), axis=0)
         # expr = divide(expr, batch_size)
-        expr = divide(expr, relax.const(batch_size, dtype=expr.struct_info.dtype))
+        expr = expr / relax.const(batch_size, dtype=_get_dtype(expr))
     return expr
 
 
@@ -449,7 +716,7 @@ def cross_entropy_without_logits_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of cross_entropy_without_logits.
 
     Forward Form:
@@ -461,7 +728,7 @@ def cross_entropy_without_logits_grad(
     """
     x, y = orig_call.args
     output_grad = _divide_batch(x, output_grad)
-    return [negative(multiply(output_grad, divide(y, x))), negative(multiply(output_grad, log(x)))]
+    return [-output_grad * y / x, -output_grad * log(x)]
 
 
 @register_gradient("relax.nn.cross_entropy_with_logits")
@@ -470,7 +737,7 @@ def cross_entropy_with_logits_grad(
     orig_call: Call,
     output_grad: Var,
     ctx: BlockBuilder,
-):
+) -> List[Expr]:
     """Gradient of cross_entropy_without_logits.
 
     Forward Form:
@@ -482,4 +749,4 @@ def cross_entropy_with_logits_grad(
     """
     x, y = orig_call.args
     output_grad = _divide_batch(x, output_grad)
-    return [negative(multiply(output_grad, y)), negative(multiply(output_grad, x))]
+    return [-output_grad * y, -output_grad * x]
