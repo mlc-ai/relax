mlc-ai/relax

commit bdba758775206855ff27a5440b1b2c1114893334

[PyPI] Add extra directories for JVM/iOS build (#230)

python/setup.py
@@ -17,11 +17,11 @@
 # pylint: disable=invalid-name, exec-used
 """Setup TVM package."""
 import os
+import pathlib
+import platform
 import shutil
 import sys
 import sysconfig
-import pathlib
-import platform
 
 from setuptools import find_packages
 from setuptools.dist import Distribution
@@ -77,6 +77,21 @@ def get_lib_path():
                 libs.append(candidate_path)
                 break
 
+        # Add 3rdparty configuration json files
+        for name in lib_path:
+            candidate_path = os.path.abspath(
+                os.path.join(os.path.dirname(name), "..", "..", "3rdparty")
+            )
+            if os.path.isdir(candidate_path):
+                libs.append(candidate_path)
+                break
+
+        # Add jvm configuration json files
+        for name in lib_path:
+            candidate_path = os.path.abspath(os.path.join(os.path.dirname(name), "..", "..", "jvm"))
+            if os.path.isdir(candidate_path):
+                libs.append(candidate_path)
+                break
     else:
         libs = None
 

commit 8379887fb34fa0221690f8729c3de53f1ab354eb

[Fix] Package path to jvm/ and 3rdparty/ (#231)
This PR fixed a bug introduced in
https://github.com/mlc-ai/relax/pull/230, where the directory is
mistakenly specified

python/setup.py
@@ -18,7 +18,6 @@
 """Setup TVM package."""
 import os
 import pathlib
-import platform
 import shutil
 import sys
 import sysconfig
@@ -77,21 +76,16 @@ def get_lib_path():
                 libs.append(candidate_path)
                 break
 
-        # Add 3rdparty configuration json files
-        for name in lib_path:
-            candidate_path = os.path.abspath(
-                os.path.join(os.path.dirname(name), "..", "..", "3rdparty")
-            )
-            if os.path.isdir(candidate_path):
-                libs.append(candidate_path)
-                break
-
-        # Add jvm configuration json files
-        for name in lib_path:
-            candidate_path = os.path.abspath(os.path.join(os.path.dirname(name), "..", "..", "jvm"))
-            if os.path.isdir(candidate_path):
-                libs.append(candidate_path)
-                break
+        for dir in ["3rdparty", "jvm", "web", "rust", "golang", "include"]:
+            for name in lib_path:
+                candidate_path = os.path.abspath(os.path.join(os.path.dirname(name), "..", dir))
+                if os.path.isdir(candidate_path):
+                    libs.append(candidate_path)
+                    if dir == "3rdparty":
+                        # remove large files
+                        shutil.rmtree(os.path.join(candidate_path, "cutlass", "docs"))
+                        shutil.rmtree(os.path.join(candidate_path, "cutlass", "media"))
+                    break
     else:
         libs = None
 