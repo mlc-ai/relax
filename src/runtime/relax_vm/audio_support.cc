8004081e03a9c0238e0d9b9e3af9dbe0db0cba68
Lesheng Jin <leshenj15@gmail.com>
2023-08-06 03:35:11 +0000

    fix

--- src/runtime/relax_vm/audio_support.cc
+++ src/runtime/relax_vm/audio_support.cc
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 /*!
  * \file src/runtime/relax_vm/audio_support.cc
  * \brief Runtime to support ASR/TTS models
