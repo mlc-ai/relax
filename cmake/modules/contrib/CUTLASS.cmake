2b204c39b53912814edc3f07e88919a5c76d00cf
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-08-08 17:21:25 -0400

    Revert "[Unity][BYOC] Integrate Flash attention v2 kernel into CUTLASS BYOC (#15467)"
    This reverts commit 38e2b886dd97cfaa0031b204441c427ac598a856.

--- cmake/modules/contrib/CUTLASS.cmake
+++ cmake/modules/contrib/CUTLASS.cmake
@@ -21,7 +21,6 @@ if(USE_CUDA AND USE_CUTLASS)
 
   set(CUTLASS_DIR ${PROJECT_SOURCE_DIR}/3rdparty/cutlass)
   add_subdirectory(${PROJECT_SOURCE_DIR}/3rdparty/cutlass_fpA_intB_gemm)
-  add_subdirectory(${PROJECT_SOURCE_DIR}/3rdparty/libflash_attn)
   list(APPEND RUNTIME_SRCS src/runtime/contrib/cutlass/weight_preprocess.cc)
 
   message(STATUS "Build with CUTLASS")
