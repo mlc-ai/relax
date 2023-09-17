c0f8b99f4c545fdcc7b6df250571bbea85172155
Junru Shao <junrushao@apache.org>
2023-09-16 21:34:30 -0700

    [CI] Require Manual Approval for the Build Stage

--- ci/jenkins/mlc_jenkinsfile.groovy
+++ ci/jenkins/mlc_jenkinsfile.groovy
@@ -113,6 +113,10 @@ def unpack_lib(name, libs) {
 
 cancel_previous_build()
 
+stage('Approval') {
+  input id: '1', message: 'Pending. Please reply "\\test" on GitHub to continue.'
+}
+
 stage('Prepare') {
   node('JUNRU-CPU-SMALL') {
     // When something is provided in ci_*_param, use it, otherwise default with ci_*
